620ed20d63f12354c8a7ef6b368152741f1b7ae0
alecxander567 <alecxanderespaldon21@gmail.com>
2026-02-10 01:30:36 +0800

    Migrated from MySQL to PostgreSQL

--- app/core/database.py
+++ app/core/database.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT
 
 DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}" f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}" f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
